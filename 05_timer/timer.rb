3c564e372c116f631452a7b99b951873e4dbb71a
Lantoniaina <manandalanalanto@gmail.com>
2018-10-09 08:22:47 +0300

    Lundi

---
+++ 05_timer/timer.rb
@@ -0,0 +1,7 @@
+#write your code here
+def time_string(x)
+	second = x % 60
+	minute = (x / 60) % 60
+	heure = (x / 60) / 60
+	return "#{heure.to_s.rjust(2, '0')}:#{minute.to_s.rjust(2, '0')}:#{second.to_s.rjust(2, '0')}"
+end
